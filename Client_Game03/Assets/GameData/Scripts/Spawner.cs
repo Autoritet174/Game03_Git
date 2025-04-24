6614b9a31ef008c7f0e0f6e09772f671849f2ccc
Autoritet174 <Autoritet174@yandex.ru>
2025-04-24 17:07:03 +0800

    1

---
+++ Client_Game03/Assets/GameData/Scripts/Spawner.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject units;
+    private void Start() {
+
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        
+    }
+}
