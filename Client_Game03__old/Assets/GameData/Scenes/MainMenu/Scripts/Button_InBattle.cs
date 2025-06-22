316a359c881086cc10a11da5d79e89bf7ea3eb78
Autoritet174 <Autoritet174@yandex.ru>
2025-06-22 22:27:14 +0800

    1

--- Client_Game03__old/Assets/GameData/Scenes/MainMenu/Scripts/Button_InBattle.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class Button_InBattle : MonoBehaviour {
-    public void OnClick() {
-        SceneManager.LoadScene("BattleField");
-    }
-}
--- Client_Game03__old/Assets/GameData/Scripts/GV.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Assets.GameData.Scripts {
-
-    /// <summary>
-    /// GlobalVariables
-    /// </summary>
-    public static class GV {
-        public static string Jwt_token;
-    }
-}
