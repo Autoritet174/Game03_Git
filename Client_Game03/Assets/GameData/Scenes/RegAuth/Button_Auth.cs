ae3182af1b1f58092b2133d1b5d456e5da1a2720
Autoritet174 <Autoritet174@yandex.ru>
2025-07-16 17:01:31 +0800

    1

--- Client_Game03/Assets/GameData/Scenes/RegAuth/Button_Auth.cs
+++ Client_Game03/Assets/GameData/Scenes/RegAuth/Button_Auth.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
 public class Button_Auth : MonoBehaviour
@@ -53,83 +54,12 @@ public class Button_Auth : MonoBehaviour
         // Данные авторизации и характеристики аппаратного устройства
         General.ModelHttp.Authorization payload = new(emailString, passwordString, SystemInfo.deviceModel, SystemInfo.deviceType.ToString(), SystemInfo.operatingSystem, SystemInfo.processorType, SystemInfo.processorCount, SystemInfo.systemMemorySize, SystemInfo.graphicsDeviceName, SystemInfo.graphicsMemorySize);
 
-
-
-        bool haveInternet = false;
-
         try
         {
-            await Task.Run(async () =>
-            {
-                //проверка интернета
-                haveInternet = await InternetChecker.CheckInternetConnectionAsync();
-
-                windowMessageController.SetTextLocale("Info.Authorization", false);
-
-                using HttpClient client = new();
-                client.Timeout = TimeSpan.FromSeconds(15);
-
-                UnityMainThreadDispatcher.RunOnMainThread(() =>
-                {
-                    Debug.Log("Device Model: " + SystemInfo.deviceModel);
-                    Debug.Log("Device Type: " + SystemInfo.deviceType);
-                    Debug.Log("Operating System: " + SystemInfo.operatingSystem);
-                    Debug.Log("Processor Type: " + SystemInfo.processorType);
-                    Debug.Log("Processor Count: " + SystemInfo.processorCount);
-                    Debug.Log("System Memory Size (MB): " + SystemInfo.systemMemorySize);
-                    Debug.Log("Graphics Device Name: " + SystemInfo.graphicsDeviceName);
-                    Debug.Log("Graphics Memory Size (MB): " + SystemInfo.graphicsMemorySize);
-                });
-
-
-
-                string json = JsonConvert.SerializeObject(payload);
-                StringContent content = new(json, Encoding.UTF8, "application/json");
-
-                try
-                {
-                    HttpResponseMessage response = await client.PostAsync(General.URLs.Uri_login, content);
-
-                    string responseContent = await response.Content.ReadAsStringAsync();
-                    JObject jObject = JObject.Parse(responseContent);
-
-                    if (!response.IsSuccessStatusCode)
-                    {
-                        windowMessageController.SetTextLocale(LocalizationManager.GetKeyError(jObject), true);
-                        return;
-                    }
-
-                    GV.Jwt_token = jObject["token"]?.ToString() ?? "";
-
-                    if (GV.Jwt_token == string.Empty)
-                    {
-                        windowMessageController.SetTextLocale("Errors.Empty_Token", true);
-                        return;
-                    }
-
-                    windowMessageController.SetTextLocale("Info.AuthorizationSuccess", true);
-                    //Scene
-                }
-                catch (TaskCanceledException ex) when (ex.InnerException is TimeoutException)
-                {
-                    windowMessageController.SetTextLocale("Errors.Server_Unavailable", true);
-                }
-                catch (HttpRequestException ex) when (ex.InnerException is WebException)
-                {
-                    if (haveInternet)
-                    {
-                        windowMessageController.SetTextLocale("Errors.No_internet_connection", true);
-                    }
-                    else
-                    {
-                        windowMessageController.SetTextLocale("Errors.Server_Unavailable", true);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    windowMessageController.SetTextErrorAndWriteExceptionInLog(ex);
-                }
-            });
+            bool success = await LoginAsync(payload);
+            if (success) {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+            }
         }
         catch (Exception ex)
         {
@@ -141,29 +71,82 @@ public class Button_Auth : MonoBehaviour
         }
     }
 
-    //public async void Test() {
-    //    using var httpClient = new HttpClient();
 
-    //    // Адрес API
-    //    var url = "https://localhost:5001/api/test"; // Убедитесь, что порт и адрес совпадают с сервером
+    private async Task<bool> LoginAsync(General.ModelHttp.Authorization payload)
+    {
+        bool haveInternet = false;
+        //проверка интернета
+        haveInternet = await InternetChecker.CheckInternetConnectionAsync();
 
-    //    // Данные для отправки
-    //    var requestData = new {
-    //        Username = "test_user",
-    //        Password = "123456"
-    //    };
+        windowMessageController.SetTextLocale("Info.Authorization", false);
 
-    //    // Отправка POST-запроса с сериализацией в JSON
-    //    var response = await httpClient.PostAsJsonAsync(url, requestData);
+        using HttpClient client = new();
+        client.Timeout = TimeSpan.FromSeconds(15);
 
-    //    // Обработка ответа
-    //    if (response.IsSuccessStatusCode) {
-    //        var result = await response.Content.ReadFromJsonAsync<TestResponse>();
-    //        Console.WriteLine($"Ответ сервера: {result?.Message}");
-    //    }
-    //    else {
-    //        Console.WriteLine($"Ошибка: {response.StatusCode}");
-    //    }
-    //}
+        UnityMainThreadDispatcher.RunOnMainThread(() =>
+        {
+            Debug.Log("Device Model: " + SystemInfo.deviceModel);
+            Debug.Log("Device Type: " + SystemInfo.deviceType);
+            Debug.Log("Operating System: " + SystemInfo.operatingSystem);
+            Debug.Log("Processor Type: " + SystemInfo.processorType);
+            Debug.Log("Processor Count: " + SystemInfo.processorCount);
+            Debug.Log("System Memory Size (MB): " + SystemInfo.systemMemorySize);
+            Debug.Log("Graphics Device Name: " + SystemInfo.graphicsDeviceName);
+            Debug.Log("Graphics Memory Size (MB): " + SystemInfo.graphicsMemorySize);
+        });
+
+
+
+        string json = JsonConvert.SerializeObject(payload);
+        StringContent content = new(json, Encoding.UTF8, "application/json");
+
+        try
+        {
+            HttpResponseMessage response = await client.PostAsync(General.URLs.Uri_login, content);
+
+            string responseContent = await response.Content.ReadAsStringAsync();
+            JObject jObject = JObject.Parse(responseContent);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                windowMessageController.SetTextLocale(LocalizationManager.GetKeyError(jObject), true);
+                return false;
+            }
+
+            GV.Jwt_token = jObject["token"]?.ToString() ?? "";
+
+            if (GV.Jwt_token == string.Empty)
+            {
+                windowMessageController.SetTextLocale("Errors.Empty_Token", true);
+                return false;
+            }
+
+            //windowMessageController.SetTextLocale("Info.AuthorizationSuccess", true);
+            
+            return true;
+        }
+        catch (TaskCanceledException ex) when (ex.InnerException is TimeoutException)
+        {
+            windowMessageController.SetTextLocale("Errors.Server_Unavailable", true);
+            return false;
+        }
+        catch (HttpRequestException ex) when (ex.InnerException is WebException)
+        {
+            if (haveInternet)
+            {
+                windowMessageController.SetTextLocale("Errors.No_internet_connection", true);
+            }
+            else
+            {
+                windowMessageController.SetTextLocale("Errors.Server_Unavailable", true);
+            }
+            return false;
+        }
+        catch (Exception ex)
+        {
+            windowMessageController.SetTextErrorAndWriteExceptionInLog(ex);
+            return false;
+        }
+    }
 
 }
