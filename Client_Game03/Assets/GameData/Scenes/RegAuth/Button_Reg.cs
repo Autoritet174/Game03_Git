15fd9f84906586b0d94dede6c732d067bbcc61fa
Autoritet174 <Autoritet174@yandex.ru>
2025-07-24 15:01:48 +0800

    1

--- Client_Game03/Assets/GameData/Scenes/RegAuth/Button_Reg.cs
+++ Client_Game03/Assets/GameData/Scenes/RegAuth/Button_Reg.cs
@@ -11,23 +11,24 @@ public class Button_Reg : MonoBehaviour
 {
     public async void OnClick()
     {
-        await Task.Run(async () =>
+        try
         {
-            using HttpClient client = new();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GV.Jwt_token);
-
-            string json = JsonConvert.SerializeObject("");
-            StringContent content = new(json, Encoding.UTF8, "application/json");
-
-            client.Timeout = TimeSpan.FromSeconds(60);
-            //_ = log.AppendLine($"{i} попытка авторизоватьс€");
-            try
+            await Task.Run(async () =>
             {
+                using HttpClient client = new();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GV.Jwt_token);
+
+                string json = JsonConvert.SerializeObject("");
+                StringContent content = new(json, Encoding.UTF8, "application/json");
+
+                client.Timeout = TimeSpan.FromSeconds(60);
+                //_ = log.AppendLine($"{i} попытка авторизоватьс€");
+
                 string s = client.ToString();
                 Assets.GameData.Scripts.Logger2.Log("Bearer: " + GV.Jwt_token);
                 HttpResponseMessage response = await client.PostAsync(General.URLs.Uri_test, content);
                 // ѕолучаем заголовки ответа в виде строки
-                string headersString = GetHeadersAsString(response);
+                string headersString = HttpHelper.GetHeadersAsString(response);
 
                 // ѕолучаем тело ответа в виде строки
                 string bodyString = await response.Content.ReadAsStringAsync();
@@ -40,38 +41,13 @@ public class Button_Reg : MonoBehaviour
                 {
                     Assets.GameData.Scripts.Logger2.Log("NOT SUCCES");
                 }
-            }
-            catch (Exception ex)
-            {
-                Assets.GameData.Scripts.Logger2.Log("[HTTP] " + ex.Message);
-            }
-
-        });
+            });
+        }
+        catch (Exception ex)
+        {
+            Assets.GameData.Scripts.Logger2.Log("[HTTP] " + ex.Message);
+        }
     }
 
-    // ћетод дл€ преобразовани€ заголовков в строку
-    private static string GetHeadersAsString(HttpResponseMessage response)
-    {
-        HttpResponseHeaders headers = response.Headers;
-        HttpContentHeaders contentHeaders = response.Content?.Headers;
-
-        string headersString = "HTTP/" + response.Version + " " + (int)response.StatusCode + " " + response.ReasonPhrase + "\n";
-
-        // ƒобавл€ем основные заголовки ответа
-        foreach (System.Collections.Generic.KeyValuePair<string, System.Collections.Generic.IEnumerable<string>> header in headers)
-        {
-            headersString += $"{header.Key}: {string.Join(", ", header.Value)}\n";
-        }
-
-        // ƒобавл€ем заголовки содержимого (если есть)
-        if (contentHeaders != null)
-        {
-            foreach (System.Collections.Generic.KeyValuePair<string, System.Collections.Generic.IEnumerable<string>> header in contentHeaders)
-            {
-                headersString += $"{header.Key}: {string.Join(", ", header.Value)}\n";
-            }
-        }
-
-        return headersString;
-    }
+    
 }
---
+++ Client_Game03/Assets/GameData/Scripts/HttpHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.GameData.Scripts
+{
+    internal static class HttpHelper
+    {/// <summary>
+     /// Метод для преобразования заголовков в строку
+     /// </summary>
+     /// <param name="response"></param>
+     /// <returns></returns>
+        internal static string GetHeadersAsString(HttpResponseMessage response)
+        {
+            HttpResponseHeaders headers = response.Headers;
+            HttpContentHeaders contentHeaders = response.Content?.Headers;
+
+            string headersString = $"HTTP/{response.Version} {(int)response.StatusCode} {response.ReasonPhrase}\n";
+
+            // Добавляем основные заголовки ответа
+            foreach (System.Collections.Generic.KeyValuePair<string, System.Collections.Generic.IEnumerable<string>> header in headers)
+            {
+                headersString += $"{header.Key}: {string.Join(", ", header.Value)}\n";
+            }
+
+            // Добавляем заголовки содержимого (если есть)
+            if (contentHeaders != null)
+            {
+                foreach (System.Collections.Generic.KeyValuePair<string, System.Collections.Generic.IEnumerable<string>> header in contentHeaders)
+                {
+                    headersString += $"{header.Key}: {string.Join(", ", header.Value)}\n";
+                }
+            }
+
+            return headersString;
+        }
+    }
+}
