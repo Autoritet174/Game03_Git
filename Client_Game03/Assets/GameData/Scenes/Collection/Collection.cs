62e7b1ee997cba4d449309e33e84989dbb26ba99
Autoritet174 <Autoritet174@yandex.ru>
2025-10-30 09:21:54 +0800

    1

--- Client_Game03/Assets/GameData/Scenes/Collection/Collection.cs
+++ Client_Game03/Assets/GameData/Scenes/Collection/Collection.cs
@@ -1,4 +1,5 @@
 using Assets.GameData.Scripts;
+using System;
 using System.ComponentModel;
 using TMPro;
 using UnityEngine;
@@ -9,11 +10,20 @@ public class Collection : MonoBehaviour
     private bool inited = false;
     private float _lastHeight;
     private float _lastWidth;
+
     RectTransform buttonHeroes;
-    TextMeshProUGUI button
+    TextMeshProUGUI buttonHeroesTmp;
+
+    RectTransform buttonItems;
+    TextMeshProUGUI buttonItemsTmp;
+
     void Start()
     {
         buttonHeroes = GameObjectFinder.FindByName<RectTransform>("ButtonHeroes (id=40jhb51a)");
+        buttonHeroesTmp = GameObjectFinder.FindByName<TextMeshProUGUI>("Text (TMP) (id=wl92ls1m)");
+
+        buttonItems = GameObjectFinder.FindByName<RectTransform>("ButtonItems (id=k5hqeyat)");
+        buttonItemsTmp = GameObjectFinder.FindByName<TextMeshProUGUI>("Text (TMP) (id=cklw2id1)");
 
 
         inited = true;
@@ -27,6 +37,10 @@ public class Collection : MonoBehaviour
         }
     }
     private void OnResize() {
-        buttonHeroes.fontSize = 66.66666f * _lastHeight / 1080;
+        _lastHeight = Screen.height;
+        _lastWidth = Screen.width;
+
+        buttonHeroesTmp.fontSize = 32f * _lastHeight / 1080;
+        buttonItemsTmp.fontSize = 32f * _lastHeight / 1080;
     }
 }
